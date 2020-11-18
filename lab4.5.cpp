dd477253eb058d2efd9d16534da28424ae6042f8
Yodelyoda <47818799+Yodelyoda@users.noreply.github.com>
2020-11-19 01:35:24 +0200

    Add files via upload

---
+++ lab4.5.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+int main()
+{
+	double x, y;
+	srand((unsigned)time(NULL));
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "x = "; cin >> x;
+		cout << "y = "; cin >> y;
+		if ((y <= x && y > 0) ||
+			(y <= pow(x, 2) && y >= -x && y <= 0))
+			cout << "yes" << endl;
+		else
+			cout << "no" << endl;
+	}
+	cout << endl << fixed;
+
+		for (int i = 0; i < 10; i++)
+		{
+			x = 4. * rand() / RAND_MAX - 2;
+			y = 4. * rand() / RAND_MAX - 2;
+			if ((y <= x && y > 0) ||
+				(y <= pow(x, 2) && y >= -x && y <= 0))
+				cout << setw(8) << setprecision(4) << x << " "
+				<< setw(8) << setprecision(4) << y << " " << "yes" << endl;
+			else
+				cout << setw(8) << setprecision(4) << x << " "
+				<< setw(8) << setprecision(4) << y << " " << "no" << endl;
+		}
+		return 0;
+}
